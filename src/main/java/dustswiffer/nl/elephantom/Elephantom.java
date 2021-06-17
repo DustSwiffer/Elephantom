21e1318800bdb69f5b85dfd22f4d8d57edf92e41
DustSwiffer <dvh_1996@hotmail.com>
2021-06-17 19:49:35 +0200

    init

---
+++ src/main/java/dustswiffer/nl/elephantom/Elephantom.java
@@ -0,0 +1,17 @@
+package dustswiffer.nl.elephantom;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Elephantom extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
